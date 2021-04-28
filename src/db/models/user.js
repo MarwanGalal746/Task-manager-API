9c6e1031084a8193a26f5e74787c44a92146687d
Marwan Galal <marawangalal84@gmail.com>
2021-04-28 02:48:57 +0200

    get methods

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
 
 const User = mongoose.model('User', {
     name: {
